e6f55f7bf1c2b1af24e6e0b42e9e095dd465bfc9
saurav2811 <saurav.sri2811@gmail.com>
2021-01-29 12:57:26 +0530

    contact form fixed and timing issue fixed

--- models/Idea.js
+++ models/Idea.js
@@ -11,7 +11,7 @@ const IdeaSchema = new Schema({
     },
     date:{
         type:Date,
-        default:Date.now
+        default:Date.now(),
     },
     user:{
         type:mongoose.Schema.Types.ObjectId,
